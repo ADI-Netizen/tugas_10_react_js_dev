3e7cf4a11f63d93b171283618972f399247cd513
ADI-Netizen <71365604+ADI-Netizen@users.noreply.github.com>
2020-10-05 18:21:38 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import Top from './component/Top';
+import Batas from './component/Batas';
+import Utama from './component/Utama'
+
+function App() {
+  return (
+    <div>
+      <Top />
+      <Batas />
+      <Utama />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Batas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Divider } from 'semantic-ui-react';
+
+function Batas() {
+    return (
+        <Divider horizontal>SELAMAT DATANG !!!</Divider>
+    )
+}
+
+export default Batas;
